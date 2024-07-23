5359b0d033b4d9f3e321eb52c6ffa4543c581f06
kleo <kleo@hak.dog>
2024-07-24 00:09:55 +0800

    new package: xfce4-cpugraph-plugin

---
+++ x11-packages/xfce4-cpugraph-plugin/build.sh
@@ -0,0 +1,12 @@
+
+TERMUX_PKG_HOMEPAGE=https://gitlab.xfce.org/panel-plugins/xfce4-cpugraph-plugin/
+TERMUX_PKG_DESCRIPTION="CPU Graph plugin for the Xfce4 panel"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.2.7
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-cpugraph-plugin/1.2/xfce4-cpugraph-plugin-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=68a651e278ed7186964e455b69b15da77f8d56257e5c3d6adf783b3ee9337405
+TERMUX_PKG_BUILD_DEPENDS="gtk3"
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_AUTO_UPDATE=false
