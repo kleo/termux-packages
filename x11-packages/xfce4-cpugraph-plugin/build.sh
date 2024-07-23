fb3072a801ecd10a6b85a8263ec69d322f16926d
kleo <kleo@hak.dog>
2024-07-24 00:24:56 +0800

    add xfce4-panel-static build dependency

--- x11-packages/xfce4-cpugraph-plugin/build.sh
+++ x11-packages/xfce4-cpugraph-plugin/build.sh
@@ -7,6 +7,6 @@ TERMUX_PKG_VERSION=1.2.7
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-cpugraph-plugin/1.2/xfce4-cpugraph-plugin-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=68a651e278ed7186964e455b69b15da77f8d56257e5c3d6adf783b3ee9337405
-TERMUX_PKG_BUILD_DEPENDS="gtk3, libxfce4panel-2.0-dev"
+TERMUX_PKG_BUILD_DEPENDS="gtk3, xfce4-panel-static"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_AUTO_UPDATE=false
